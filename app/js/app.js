1ff3511382f4856416cb7e6c827ac6c6b7d821cd
Max Mulatz <klappradla@gmail.com>
2015-02-03 19:38:14 +0100

    render word with letters

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,20 @@
 var app = {
+	
 	init: function () {
-		var canvas = Raphael(0, 0, 600, 400);
+		var playground = new Playground();
+		var word = new Word("quick", playground.canvas);
+		//var testLetter = new Letter('m', playground.canvas);
 
-		var x = 0, y = 0;
+		// var x = 0, y = 0;
 
-		var fontFamilies = [];
-		fontFamilies.push('serif', 'sans-serif', 'cursive', 'fantasy', 'monospace');
-		fontFamilies.forEach(function (fontFamily) {
-			var textComp = new TextComponent(canvas, fontFamily);
-			textComp.render();
-			textComp.transform(x, y);
-			x += 70;
-			y += 30;
-		})
+		// var fontFamilies = [];
+		// fontFamilies.push('serif', 'sans-serif', 'cursive', 'fantasy', 'monospace');
+		// fontFamilies.forEach(function (fontFamily) {
+		// 	var textComp = new TextComponent(playground.canvas, fontFamily);
+		// 	textComp.render();
+		// 	textComp.transform(x, y);
+		// 	x += 70;
+		// 	y += 30;
+		// })
 	}
 }
